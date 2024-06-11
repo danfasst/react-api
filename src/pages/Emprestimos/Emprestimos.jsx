4eb69c56fd0df03c685a284c01d38a4ee36e747b
danfasst <danielcfcunha2005@gmail.com>
2024-06-11 09:03:33 -0300

    Usuario e Livro

--- src/pages/Emprestimos/Emprestimos.jsx
+++ src/pages/Emprestimos/Emprestimos.jsx
@@ -1,13 +1,12 @@
 import "./Emprestimo.css";
 import axios from 'axios';
-import { useState, useEffect } from 'react';
-import { useFetcher } from "react-router-dom";
+import { useState, useEffect, useInsertionEffect } from 'react';
 
 
 function Emprestimos() {
-
-
-    const[emprestimos, setEmprestimos] = useState([]);
+    const [emprestimos, setEmprestimos] = useState([]);
+    const [usuarios, setUsuarios] = useState([]);
+    const [livros, setLivros] = useState([]);
 
     function getEmprestimos() {
         axios.get("http://localhost:5073/emprestimos").then((resposta) => {
@@ -15,14 +14,28 @@ function Emprestimos() {
         });
     }
 
-    function excluirEmprestimo(id){
-        axios.delete("http://localhost:5073/emprestimos/" + id)
-        .then(()=>{
-        getEmprestimos();
+    function getUsuarios() {
+        axios.get("http://localhost:5073/usuarios").then((resposta) => {
+            setUsuarios(resposta.data);
         });
     }
 
-    useEffect(getEmprestimos, []);  
+    function getLivros() {
+        axios.get("http://localhost:5073/livros").then((resposta) => {
+            setLivros(resposta.data)
+        });
+    }
+
+    function excluirEmprestimo(id) {
+        axios.delete("http://localhost:5073/emprestimos/" + id)
+            .then(() => {
+                getEmprestimos();
+            });
+    }
+
+    useEffect(getEmprestimos, []);
+    useEffect(getUsuarios, []);
+    useEffect(getLivros, []);
 
     return (
         <>
@@ -35,8 +48,7 @@ function Emprestimos() {
                 <input placeholder="Data Emprestimo" type="text" id="dataEmprestimo" />
                 <br />
                 <input placeholder="Data de Devolucao" type="text" id="dataDevolucao" />
-                
-                
+
                 <button type="submit">Fazer Empréstimo</button>
             </form>
             {getTabela()}
@@ -45,51 +57,47 @@ function Emprestimos() {
 
     function getLinha(id, livros, usuario, dataEmprestimo, dataDevolucao) {
         return (
-            <tr>
+            <tr key={id}>
                 <td>{id}</td>
                 <td>{livros}</td>
                 <td>{usuario}</td>
                 <td>{dataEmprestimo}</td>
                 <td>{dataDevolucao}</td>
                 <td>
-                    <button onClick={() => { excluirEmprestimo(id); }}>Excluir</button>
+                    <button onClick={() => excluirEmprestimo(id)}>Excluir</button>
                     <button>Editar</button>
                 </td>
             </tr>
         );
-    }   
+    }
 
     function getLinhas() {
-
-        const linhasDaTabela = [];
-
-        for(let i = 0; i < emprestimos.length; i++) {
-
-            const emprestimo = emprestimos[i];
-
-            linhasDaTabela [i] = getLinha(emprestimo.id, emprestimo.livros, emprestimo.usuario, 
-            emprestimo.dataEmprestimo, emprestimo.dataDevolucao);
-        }
-
-        return linhasDaTabela;
+        return emprestimos.map((emprestimo) => {
+            const usuario = usuarios.find((usuario) => usuario.id > 0);
+            const livro = livros.find((livro) => livro.nome);
+            return getLinha(emprestimo.id, livro.nome, usuario.id, emprestimo.dataEmprestimo, emprestimo.dataDevolucao);
+        });
     }
 
     function getTabela() {
         return (
             <table>
-                <tr>
-                    <th>ID</th>
-                    <th>Livro</th>
-                    <th>Usuario</th>
-                    <th>Data de Emprestimo</th>
-                    <th>Data de Devolucao</th>
-                    <th>Ações</th>
-                </tr>
-                {getLinhas()}
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Livro</th>
+                        <th>Usuario</th>
+                        <th>Data de Emprestimo</th>
+                        <th>Data de Devolucao</th>
+                        <th>Ações</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {getLinhas()}
+                </tbody>
             </table>
         );
     }
-
 }
 
 export default Emprestimos;
